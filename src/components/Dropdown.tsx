0b7d4511f8d1a34318a646d5eeb62d7b35a804d7
duartrosar <74115478+duartrosar@users.noreply.github.com>
2023-07-28 12:23:31 +0100

    Font type toggler

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useState, useRef, MouseEvent as ReactMouseEvent } from "react";
+import { IoChevronDownSharp, IoCheckmarkSharp } from "react-icons/io5";
+import { useClickAway } from "react-use";
+
+const Dropdown = () => {
+  const [selectedText, setSelectedText] = useState("");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useClickAway(dropdownRef, (e: MouseEvent) => {
+    const element = e.target as HTMLElement;
+    const id = element.id;
+    const attr = element.getAttribute("data-toggler");
+
+    if (attr === "toggler") return;
+
+    setDropdownOpen(false);
+  });
+
+  function handleClick(e: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+    const element = e.target as HTMLElement;
+    const text = element.innerHTML;
+    setDropdownOpen(false);
+    setSelectedText(text);
+  }
+
+  return (
+    <div className="relative flex flex-col gap-1">
+      <div
+        className="flex items-center justify-end gap-4 px-4 border-r cursor-pointer border-dark-300 min-w-[147px]"
+        id="toggler"
+        data-toggler="toggler"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <p data-toggler="toggler" className="text-sm font-bold leading-6">
+          {selectedText}
+        </p>
+        <IoChevronDownSharp
+          data-toggler="toggler"
+          size={18}
+          className="text-primary"
+        />
+      </div>
+      {dropdownOpen && (
+        <div
+          ref={dropdownRef}
+          className="absolute left-0 z-50 w-full bg-white border rounded-lg shadow-md dark:bg-dark-700 border-dark-300 top-8 dark:border-dark-500"
+        >
+          <ul className="flex flex-col gap">
+            <li
+              onClick={(e) => handleClick(e)}
+              className="flex items-center justify-between w-full px-4 text-sm font-bold leading-6 transition cursor-pointer hover:bg-dark-200 dark:hover:bg-dark-600 hover:shadow-md"
+            >
+              Sans Serif
+            </li>
+            <li
+              onClick={(e) => handleClick(e)}
+              className="flex items-center justify-between w-full px-4 text-sm font-bold leading-6 transition cursor-pointer hover:bg-dark-200 dark:hover:bg-dark-600 hover:shadow-md"
+            >
+              Serif
+            </li>
+            <li
+              onClick={(e) => handleClick(e)}
+              className="flex items-center justify-between w-full px-4 text-sm font-bold leading-6 transition cursor-pointer hover:bg-dark-200 dark:hover:bg-dark-600 hover:shadow-md"
+            >
+              Monospace
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import logo from "../../public/logo.svg";
-import { IoChevronDownSharp } from "react-icons/io5";
 import { PiMoonLight } from "react-icons/pi";
 import Toggle from "./Toggle";
+import Dropdown from "./Dropdown";
 
 const Navbar = () => {
   return (
@@ -10,10 +10,7 @@ const Navbar = () => {
       <div className="flex justify-between max-w-[736px] mx-auto">
         <Image alt="logo" src={logo} width={28.05} height={32} />
         <div className="flex">
-          <div className="flex items-center gap-4 pr-4 border-r border-dark-300 dark:border-">
-            <p className="text-sm font-bold leading-6">Sans Serif</p>
-            <IoChevronDownSharp size={18} className="text-primary" />
-          </div>
+          <Dropdown />
           <div className="flex items-center gap-3 pl-4">
             <Toggle />
             <PiMoonLight size={20} className="dark:text-primary" />
