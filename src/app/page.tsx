bc6f3c51403b8dffe6e3ff6acbc73aa42170fe18
Duarte Ribeiro <duartrosar@gmail.com>
2023-08-04 12:45:42 +0100

    Fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-import Word from "@/components/Word";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
   const encodedSearchQuery = encodeURI("keyboard");
 
-  router.push(`/search/${encodedSearchQuery}`);
-  return <></>;
+  redirect(`/search/${encodedSearchQuery}`);
 }
